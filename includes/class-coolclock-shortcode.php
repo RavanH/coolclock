e951b7516fc8dd91d2a8040a5f3bf23b6eaca630
RavanH <ravanhagen@gmail.com>
2019-05-06 22:50:34 +0200

    sanitize skin names and allow custom parameters in shortcode

--- includes/class-coolclock-shortcode.php
+++ includes/class-coolclock-shortcode.php
@@ -6,33 +6,30 @@
 
 class CoolClock_Shortcode {
 
-	public static function handle_shortcode( $atts, $content = null ) {
+	public static function handle_shortcode( $atts, $content = null )
+	{
 		if ( is_feed() )
 			return '';
 
 		$atts = shortcode_atts( array_merge( CoolClock::$defaults, CoolClock::$advanced_defaults ), $atts, 'coolclock' );
 
-		// TODO user input (attributes) validation here !!
-		/*
-		skin -- must be one of these: 'swissRail' (default skin), 'chunkySwiss', 'chunkySwissOnBlack', 'fancy', 'machine', 'simonbaird_com', 'classic', 'modern', 'simple', 'securephp', 'Tes2', 'Lev', 'Sand', 'Sun', 'Tor', 'Cold', 'Babosa', 'Tumb', 'Stone', 'Disc', 'watermelon' or 'mister'. If the Advanced extension is activated, there is also 'minimal' available. Please note that these names are case sensitive.
-		radius -- a number to define the clock radius. Do not add 'px' or any other measure descriptor.
-		noseconds -- set to true (or 1) to hide the second hand
-		gmtoffset -- a number to define a timezone relative the Greenwhich Mean Time. Do not set this parameter to default to local time.
-		showdigital -- set to 'digital12' to show the time in 12h digital format (with am/pm) too
-		digitalcolor -- set to a color value to change the digital time color
-		scale -- must be one of these: 'linear' (default scale), 'logClock' or 'logClockRev'. Linear is our normal clock scale, the other two show a logarithmic time scale
-		subtext -- optional text, centered below the clock
-		align -- sets floating of the clock: 'left', 'right' or 'center'
+		/**
+		* skin					Must be one of these: 'swissRail' (default skin), 'chunkySwiss', 'chunkySwissOnBlack', 'fancy', 'machine', 'simonbaird_com', 'classic', 'modern', 'simple', 'securephp', 'Tes2', 'Lev', 'Sand', 'Sun', 'Tor', 'Cold', 'Babosa', 'Tumb', 'Stone', 'Disc', 'watermelon' or 'mister'.
+		* 							If the Advanced extension is activated, there is also 'minimal' available.
+		* radius				A number to define the clock radius. Do not add 'px' or any other measure descriptor.
+		* noseconds			Set to true (or 1) to hide the second hand
+		* gmtoffset			A number to define a timezone relative the Greenwhich Mean Time. Do not set this parameter to default to local time.
+		* showdigital		Set to 'digital12' to show the time in 12h digital format (with am/pm) too
+		* digitalcolor	Set to a color value to change the digital time color
+		* scale					Must be one of these: 'linear' (default scale), 'logClock' or 'logClockRev'. Linear is our normal clock scale, the other two show a logarithmic time scale
+		* subtext				Optional text, centered below the clock
+		* align					Sets floating of the clock: 'left', 'right' or 'center'
 		*/
 
 		// set footer script flags
 		CoolClock::$add_script = true;
 
-		if ( !isset( $atts['skin'] ) )
-			$atts['skin'] = CoolClock::$defaults['skin'];
-
-		if ( in_array( $atts['skin'], CoolClock::$more_skins ) )
-			CoolClock::$add_moreskins = true;
+		$atts['skin'] = !empty($atts['skin']) ? self::parse_skin( $atts['skin'] ) : CoolClock::$defauls['skin'];
 
 		// get output
 		$output = CoolClock::canvas( $atts );
@@ -40,7 +37,38 @@ class CoolClock_Shortcode {
 		return apply_filters( 'coolclock_shortcode_advanced', $output, $atts, $content );
 	}
 
-	public static function no_wptexturize($shortcodes) {
+	private static function parse_skin( $skin_name, $content )
+	{
+		// look trhough the default skins first
+		foreach ( CoolClock::$default_skins as $skin ) {
+			if ( strtolower($skin_name) == strtolower($skin) )
+			// return the matching skin
+			return $skin;
+		}
+
+		// still here? then search in the $more_skins array
+		foreach ( CoolClock::$more_skins as $skin ) {
+			if ( strtolower($skin_name) == strtolower($skin) ) {
+				// set more_skins flag
+				CoolClock::$add_moreskins = true;
+				// return the matching skin
+				return $skin;
+			}
+		}
+
+		// still here? then skin is a custom skin
+
+		// add custom skin parameters to the advanced skins array
+		if ( !in_array( $atts['skin'], CoolClock::$advanced_skins ) && !empty( $content ) ) {
+			CoolClock::$advanced_skins[] = $atts['skin'];
+			CoolClock::$advanced_skins_config[$atts['skin']] = wp_strip_all_tags( $content, true );
+		}
+
+		return $skin_name;
+	}
+
+	public static function no_wptexturize( $shortcodes )
+	{
 		$shortcodes[] = 'coolclock';
 		return $shortcodes;
 	}
